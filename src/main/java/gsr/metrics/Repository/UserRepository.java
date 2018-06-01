882c0b0e2bcabf60e4073cf3e87865a6c9d2a53c
Kirtley <kevin.kirtley@gmail.com>
2018-06-01 15:05:59 -0500

    Created repo method using method signature

--- src/main/java/gsr/metrics/Repository/UserRepository.java
+++ src/main/java/gsr/metrics/Repository/UserRepository.java
@@ -3,7 +3,9 @@ package gsr.metrics.Repository;
 import gsr.metrics.model.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User,Long>{
+import java.util.List;
 
+public interface UserRepository extends CrudRepository<User,Long>{
+	List<User> findDistinctByLastNameIgnoreCase(String last_name);
 }
 
